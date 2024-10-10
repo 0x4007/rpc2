3ab1045ae1fa69c3a3c56f3807d9603186e9fda4
アレクサンダー.eth <gpg@pavlovcik.com>
2024-10-11 05:18:40 +0900

    feat: ui

--- static/main.ts
+++ static/main.ts
@@ -1,19 +1,24 @@
 import chains from "../src/fixtures/chains.json";
 import { ChainData, RpcHandler } from "../src/rpc-handler";
-export async function mainModule() {
+
+export async function callRpc(id: number, method = "eth_blockNumber", params = []) {
   const rpcHandler = new RpcHandler(chains as ChainData[]);
-  const response = await rpcHandler.sendRequest({
-    jsonrpc: "2.0",
-    method: "eth_blockNumber",
-    params: [],
-    id: 100,
-  });
+  const response = await rpcHandler.sendRequest({ jsonrpc: "2.0", method, params, id });
   console.log(response); // Changed from console.trace to console.log
   const result = parseInt(response.result, 16);
   const buffer = [`eth_blockNumber: ${result}`].join("\n\n");
   document.getElementById("output").innerText = buffer;
-
   return rpcHandler;
 }
 
-void mainModule();
+const networkIdInput = document.getElementById("network-id-input") as HTMLInputElement;
+
+const callRpcButton = document.getElementById("call-rpc-button");
+callRpcButton.addEventListener("click", async () => {
+  const networkIdSelection = networkIdInput.value;
+  if (!networkIdSelection) {
+    console.error("Network ID is required");
+    return;
+  }
+  await callRpc(networkIdSelection);
+});
